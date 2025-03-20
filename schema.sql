e1327912e8af5d194be52e2f898ccad5c844e238
GMar25 <gagemariano@gmail.com>
2025-03-20 03:01:56 -0500

    Implement basic schema

---
+++ schema.sql
@@ -0,0 +1,86 @@
+/* Entities */
+CREATE TABLE Participants (
+    email CHAR(30) PRIMARY KEY,
+    name CHAR(30) NOT NULL,
+    major CHAR(30), --Participant not necessarily a student
+    classification CHAR(10)
+);
+
+CREATE TABLE Judges(
+    id INT PRIMARY KEY,
+    name CHAR(30),
+    field CHAR(50)
+);
+
+CREATE TABLE Events(
+    id INT PRIMARY KEY,
+    name CHAR(50) NOT NULL,
+    host CHAR(50) NOT NULL,
+    event_date date NOT NULL,
+    duration int NOT NULL --hours 
+        CHECK (duration >= 0)
+);
+
+CREATE TABLE Prizes(
+    id INT PRIMARY KEY,
+    placement int NOT NULL
+        CHECK (placement > 0),
+    amount int
+        DEFAULT 0
+        CHECK(amount >= 0), --dollars
+    sponsor CHAR(50)
+);
+
+CREATE TABLE Projects (
+    link CHAR(50) PRIMARY KEY,
+    name CHAR(50), --Not all attributes would necessarily be required by an event for submission
+    field CHAR(50),
+    description CHAR(200),
+    event_id INT NOT NULL REFERENCES Events(id)
+);
+
+CREATE TABLE Workshops(
+    id INT PRIMARY KEY,
+    name CHAR(50),
+    host CHAR(30),
+    field CHAR(50) NOT NULL, 
+    event_id INT NOT NULL REFERENCES Events(id)
+);
+
+/* Relationships */
+-- Participants to Projects (many to many)
+CREATE TABLE WorkedOn( 
+    email CHAR(30),
+    link CHAR(50),
+    PRIMARY KEY(email, link),
+    FOREIGN KEY(email) REFERENCES Participants(email),
+    FOREIGN KEY(link) REFERENCES Projects(link)
+);
+
+-- Participants to Workshops (many to many)
+CREATE TABLE Visited(
+    email CHAR(30),
+    workshop_id INT,
+    PRIMARY KEY(email, workshop_id),
+    FOREIGN KEY(email) REFERENCES Participants(email),
+    FOREIGN KEY(workshop_id) REFERENCES Workshops(id)
+);
+
+-- Projects to Judges (many to many)
+CREATE TABLE Reviewed(
+    link CHAR(50),
+    judge_id INT,
+    score INT NOT NULL,
+    PRIMARY KEY(link, judge_id),
+    FOREIGN KEY(link) REFERENCES Projects(link),
+    FOREIGN KEY(judge_id) REFERENCES Judges(id)
+);
+
+-- Prizes to Events (many to many)
+CREATE TABLE AwardedAt(
+    prize_id INT,
+    event_id INT,
+    PRIMARY KEY(prize_id, event_id),
+    FOREIGN KEY(prize_id) REFERENCES Prizes(id),
+    FOREIGN KEY(event_id) REFERENCES Events(id)
+);
