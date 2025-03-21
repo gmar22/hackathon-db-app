8c77f108b6e11058338ba0a2efdc0100355fa84f
GMar25 <gagemariano@gmail.com>
2025-03-20 21:05:39 -0500

    Correct path

--- server/index.js
+++ server/index.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Middleware
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../static')));
 
 // Default route
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+  res.sendFile(path.join(__dirname, '../static', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
